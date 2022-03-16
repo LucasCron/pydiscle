07e52e1d60fa1c660c6b50bc93d78b29d67c70d2
Lucas Cron <lu@LuMac.local>
2022-03-16 00:27:16 -0400

    new db with way more discs, add discraft only option, add discraft ratings

--- src/main.py
+++ src/main.py
@@ -1,25 +1,26 @@
 import csv
 import random
 
-Y = "y"
-DOWN = "v"
-UP = "^"
-GREY = "grey"
-GREEN = "green"
-MANUS = "manufacturers"
+Y = 'y'
+DOWN = 'v'
+UP = '^'
+GREY = 'grey'
+GREEN = 'green'
+MANUS = 'manufacturers'
 
-NUM = "num"
-NAME = "name"
-MANU = "manufacturer"
-SP = "speed"
-GL = "glide"
-TURN = "turn"
-FADE = "fade"
-TYPE = "type"
+NUM = 'num'
+NAME = 'name'
+MANU = 'manufacturer'
+SP = 'speed'
+GL = 'glide'
+TURN = 'turn'
+FADE = 'fade'
+D_RATING = 'discraft rating'
+TYPE = 'type'
 
-info_columns = [MANU, SP, GL, TURN, FADE, TYPE]
+info_columns = [MANU, SP, GL, TURN, FADE, D_RATING, TYPE]
 
-db_version = 4
+db_version = 5
 max_guesses = 6
 
 identifier_padding = 8
@@ -61,7 +62,10 @@ def read_disc_db(file):
 
 
 def compare_num(actual, disc_guess) -> str:
-    # converting strings to numerics
+    # handling for empty Discraft ratings
+    if actual is None or disc_guess is None:
+        return None
+
     num_actual = float(actual)
     num_guess = float(disc_guess)
     if num_actual == num_guess:
@@ -86,59 +90,94 @@ def pad(val: str, size=num_padding):
     return str(val).ljust(size, ' ')
 
 
-def print_stat_output(identifier, man, speed, glide, turn, fade, disc_type):
+def get_d_rating(discraft_only_game, disc_entry):
+    return disc_entry.get(D_RATING) if discraft_only_game else None
+
+
+def print_stat_output(identifier, man, speed, glide, turn, fade, disc_type, drating):
+    drating_str = ''
+    if drating is not None:
+        drating_str = f'{D_RATING}:{drating}'
     print(f'{pad(identifier, identifier_padding)} = {MANU}:{pad(man, string_padding)}'
           f'{SP}:{pad(speed)}'
           f'{GL}:{pad(glide)}'
           f'{TURN}:{pad(turn)}'
           f'{FADE}:{pad(fade)}'
-          f'{TYPE}:{disc_type}')
+          f'{TYPE}:{pad(disc_type, 10)}'
+          f'{drating_str}')
 
 
 if __name__ == '__main__':
-    lookup = read_disc_db(f'discLookups/discs-db-{db_version}.csv')
+    file_name = f'src/discLookups/discs-db-{db_version}.csv'
+    lookup = read_disc_db(file_name)
     disc = random.choice(list(lookup))
+    include_list = []
 
-    exclude_manufacturers = input("exclude manufacturers? type y to exclude: ")
-    exclude_list = []
-    if exclude_manufacturers == Y:
-        for manufacturer in lookup[MANUS]:
-            exclude = input(f"exclude {manufacturer}? type y to exclude: ")
-            if exclude == Y:
-                exclude_list.append(manufacturer)
+    # short circuit if only want to do Discraft game, since that is my favorite :)
+    discraft_only = False
+    discraft_only_input = input('discraft only? type y to guess from only discraft: ')
+    if discraft_only_input == Y:
+        discraft_only = True
+        include_list.append('discraft')
 
-    while lookup[disc][MANU] in exclude_list:
+    # create include list of manufacturers, can still end up with a Discraft only game
+    if not discraft_only:
+        include_manufacturers = input('specific manufacturers? type y to choose: ')
+        if include_manufacturers == Y:
+            for manufacturer in lookup[MANUS]:
+                exclude = input(f'include {manufacturer}? type y to include: ')
+                if exclude == Y:
+                    include_list.append(manufacturer)
+        if len(include_list) == 1 and 'discraft' in include_list:
+            discraft_only = True
+
+    # getting random disc until found in include list (not optimal, oh well)
+    while len(include_list) > 0 and lookup[disc][MANU] not in include_list:
         disc = random.choice(list(lookup))
+
+    actual = lookup[disc]
+    actual_drating = get_d_rating(discraft_only, actual)
 
     correct = False
     guesses = 1
-    print("note: disc types are defined by speed: putter 1-3, mid 4-5, fairway 6-8, distance 9-14")
+    print('note: disc types are defined by speed: putter 1-3, mid 4-5, fairway 6-8, distance 9-14')
     while not correct and guesses < max_guesses + 1:
         inp = input(f'guess {guesses} -> ')
         if lookup.get(inp) is None:
-            print("not a disc... guess again!")
+            print('not a disc... guess again!')
         elif inp == disc:
             print(f'\nyou win! it took you {guesses} guesses')
             correct = True
         else:
             guesses += 1
-            print("wrong, guess again!")
+            print('wrong, guess again!')
 
             guess = lookup[inp]
-            print_stat_output("guess", guess[MANU], guess[SP], guess[GL], guess[TURN], guess[FADE], guess[TYPE])
-            print_stat_output("actual",
-                              compare_str(lookup[disc][MANU], lookup[inp][MANU]),
-                              compare_num(lookup[disc][SP], lookup[inp][SP]),
-                              compare_num(lookup[disc][GL], lookup[inp][GL]),
-                              compare_num(lookup[disc][TURN], lookup[inp][TURN]),
-                              compare_num(lookup[disc][FADE], lookup[inp][FADE]),
-                              compare_str(lookup[disc][TYPE], lookup[inp][TYPE]))
+            guess_drating = get_d_rating(discraft_only, guess)
+
+            print_stat_output('guess',
+                              guess[MANU],
+                              guess[SP],
+                              guess[GL],
+                              guess[TURN],
+                              guess[FADE],
+                              guess[TYPE],
+                              guess_drating)
+            print_stat_output('actual',
+                              compare_str(actual[MANU], guess[MANU]),
+                              compare_num(actual[SP], guess[SP]),
+                              compare_num(actual[GL], guess[GL]),
+                              compare_num(actual[TURN], guess[TURN]),
+                              compare_num(actual[FADE], guess[FADE]),
+                              compare_str(actual[TYPE], guess[TYPE]),
+                              compare_num(actual_drating, guess_drating))
 
     if guesses > max_guesses:
-        print("\nyou failed!")
-        print("it was the " + disc)
+        print('\nyou failed!')
+        print('it was the ' + disc)
 
-    print_stat_output("stats",
-                      lookup[disc][MANU], lookup[disc][SP],
-                      lookup[disc][GL], lookup[disc][TURN],
-                      lookup[disc][FADE], lookup[disc][TYPE])
+    print_stat_output('stats',
+                      actual[MANU], actual[SP],
+                      actual[GL], actual[TURN],
+                      actual[FADE], actual[TYPE],
+                      actual_drating)
